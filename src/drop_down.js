3c5cef362326797031be1caf7c1e41b5c114bf3d
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-08 14:28:11 -0700

    Phase 2 done, instruction still too long didn't read

--- src/drop_down.js
+++ src/drop_down.js
@@ -9,3 +9,23 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+export const dogDropDown = () => {
+  const dropDownList = document.getElementById("drop-down-dog-list");
+  const dropDownBox = document.getElementById("drop-down-dog-nav");
+  dropDownBox.addEventListener("mouseenter", event => {
+    console.log("hi")
+    dropDownList.classList.add("open");
+  });
+  dropDownBox.addEventListener("mouseleave", event => {
+    console.log("bye")
+    dropDownList.classList.remove("open");
+  });
+  Object.entries(dogs).forEach(([name, link]) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = link;
+    a.text = name;
+    li.append(a);
+    dropDownList.append(li);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "../css/styles.scss"
 import warmUp from "./warmup";
 import Clock from "./clock";
+import { dogDropDown } from "./drop_down"
 
 const clock = new Clock();
+dogDropDown();
