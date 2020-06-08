fe61d59de11e15046745fa84be31d517529b23e6
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-08 11:46:22 -0700

    Phase 0

--- src/warmup.js
+++ src/warmup.js
@@ -1,8 +1,16 @@
 
 const partyHeader = document.getElementById('party');
 
+/**
+ * The desired behavior of this function is the input string will be converted into a <p> tag and then appended to the
+ * input HTML Element.
+ * @param {String} string
+ * @param {HTMLElement} htmlElement
+ */
 export const htmlGenerator = (string, htmlElement) => {
-
+  const element = document.createElement("p");
+  element.innerText = string;
+  htmlElement.append(element);
 };
 
 htmlGenerator('Party Time.', partyHeader);
